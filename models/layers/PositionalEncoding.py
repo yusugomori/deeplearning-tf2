4f985f172b35adcce0e22bf2ce5c48c24c442c5e
Yusuke Sugomori <me@yusugomori.com>
2019-03-05 00:21:48 +0900

    fix PositionalEncoding

--- models/layers/PositionalEncoding.py
+++ models/layers/PositionalEncoding.py
@@ -10,7 +10,6 @@ class PositionalEncoding(Layer):
     def __init__(self, output_dim,
                  max_len=6000,
                  **kwargs):
-        self.supports_masking = True
         self.output_dim = output_dim
         self.max_len = max_len
         super().__init__(**kwargs)
@@ -20,23 +19,28 @@ class PositionalEncoding(Layer):
                                   shape=(self.max_len,
                                          self.output_dim),
                                   initializer=self.initializer,
-                                  trainable=False)
+                                  trainable=False,
+                                  dtype=tf.float32)
 
         super().build(input_shape)
 
     def call(self, x, mask=None):
         pe = self.PE[tf.newaxis, :tf.shape(x)[1], :]
+        if mask is not None:
+            pe = tf.tile(pe, [mask.shape[0], 1, 1])
+            pe *= tf.cast(mask, tf.float32)[:, :, tf.newaxis]
         return x + pe
 
     def initializer(self, input_shape, dtype=tf.float32):
-        pe = np.zeros(shape=input_shape, dtype=np.float32)
-        pos = np.arange(0, self.max_len)[:, np.newaxis]
-        div = np.power(10000,
-                       np.arange(0, self.output_dim, 2) / self.output_dim)
-        pe[:, 0::2] = np.sin(pos / div)
-        pe[:, 1::2] = np.cos(pos / div)
+        pe = \
+            np.array([[pos / np.power(10000, 2 * (i // 2) / self.output_dim)
+                       for i in range(self.output_dim)]
+                      for pos in range(self.max_len)])
 
-        return pe
+        pe[:, 0::2] = np.sin(pe[:, 0::2])
+        pe[:, 1::2] = np.sin(pe[:, 1::2])
+
+        return tf.convert_to_tensor(pe, dtype=tf.float32)
 
     def compute_mask(self, inputs, mask):
         return mask
