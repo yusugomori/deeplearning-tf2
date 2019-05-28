2553667a5466a067c8fb6ff4c0110579e53f5d6b
jeffin07 <francisjeffin07@gmail.com>
2019-05-28 12:17:09 +0530

    changes after review

--- models/vgg16_mnist_beginner.py
+++ models/vgg16_mnist_beginner.py
@@ -1,65 +1,64 @@
 import tensorflow as tf 
 from tensorflow.keras.models import Model
-from tensorflow.keras.losses import MSE
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import \
+	Conv2D, Dense, MaxPool2D, Flatten, Input
 import numpy as np
+from sklearn.utils import shuffle
 
-class Vgg16(tf.keras.Model):
+class Vgg16(Model):
 	def __init__(self,output_nodes):
 		super(Vgg16, self).__init__()
-		# layers needed
-		self.conv1_1 = tf.keras.layers.Conv2D(
+		self.conv1_1 = Conv2D(
 					input_shape=[None,28,28,1],filters=64, kernel_size=3,
-					padding="same", activation="relu")
-		self.conv1_2 = tf.keras.layers.Conv2D(
+					padding='same', activation='relu')
+		self.conv1_2 = Conv2D(
 					filters=64, kernel_size=3,
-					padding="same", activation="relu")
-		self.conv2_1 = tf.keras.layers.Conv2D(
+					padding='same', activation='relu')
+		self.conv2_1 = Conv2D(
 					filters=128, kernel_size=3,
-					padding="same", activation="relu")
-		self.conv2_2 = tf.keras.layers.Conv2D(
+					padding='same', activation='relu')
+		self.conv2_2 = Conv2D(
 					filters=128, kernel_size=3,
-					padding="same", activation="relu")
-		self.conv3_1 = tf.keras.layers.Conv2D(
+					padding='same', activation='relu')
+		self.conv3_1 = Conv2D(
 					filters=256,kernel_size=3,
-					padding="same", activation="relu")
-		self.conv3_2 = tf.keras.layers.Conv2D(
+					padding='same', activation='relu')
+		self.conv3_2 = Conv2D(
 					filters=256,kernel_size=3,
-					padding="same", activation="relu")
-		self.conv3_3 = tf.keras.layers.Conv2D(
+					padding='same', activation='relu')
+		self.conv3_3 = Conv2D(
 					filters=256,kernel_size=3,
-					padding="same", activation="relu")
+					padding='same', activation='relu')
 		
-		self.conv4_1 = tf.keras.layers.Conv2D(
+		self.conv4_1 = Conv2D(
 					filters=512, kernel_size=3,
-					padding="same", activation="relu")
-		self.conv4_2 = tf.keras.layers.Conv2D(
+					padding='same', activation='relu')
+		self.conv4_2 = Conv2D(
 					filters=512, kernel_size=3,
-					padding="same", activation="relu")
-		self.conv4_3 = tf.keras.layers.Conv2D(
+					padding='same', activation='relu')
+		self.conv4_3 = Conv2D(
 					filters=512, kernel_size=3,
-					padding="same", activation="relu")
-		self.conv5_1 = tf.keras.layers.Conv2D(
+					padding='same', activation='relu')
+		self.conv5_1 = Conv2D(
 					filters=512, kernel_size=3,
-					padding="same", activation="relu")
-		self.conv5_2 = tf.keras.layers.Conv2D(
+					padding='same', activation='relu')
+		self.conv5_2 = Conv2D(
 					filters=512, kernel_size=3,
-					padding="same", activation="relu")
-		self.conv5_3 = tf.keras.layers.Conv2D(
+					padding='same', activation='relu')
+		self.conv5_3 = Conv2D(
 					filters=512, kernel_size=3,
-					padding="same", activation="relu")
-		self.dense1_1 = tf.keras.layers.Dense(
-					units=4096, activation="relu")
-		self.dense1_2 = tf.keras.layers.Dense(
-					units=4096, activation="relu")
-		self.dense2 = tf.keras.layers.Dense(
-					units=output_nodes, activation="softmax")
-		self.maxPool = tf.keras.layers.MaxPool2D(
-					pool_size=2, strides=2, padding="same")
-		self.flatten = tf.keras.layers.Flatten()
+					padding='same', activation='relu')
+		self.dense1_1 = Dense(
+					units=4096, activation='relu')
+		self.dense1_2 = Dense(
+					units=4096, activation='relu')
+		self.dense2 = Dense(
+					units=output_nodes, activation='softmax')
+		self.maxPool = MaxPool2D(
+					pool_size=2, strides=2, padding='same')
+		self.flatten = Flatten()
 
 	def call(self,input):
-		# ops 
 		x = self.conv1_1(input)
 		x = self.conv1_2(x)
 		x = self.maxPool(x)
@@ -85,34 +84,89 @@ class Vgg16(tf.keras.Model):
 		return x
 
 
-network = Vgg16(10)
-# network.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+if __name__ == '__main__':
+	
+	@tf.function
+	def compute_loss(label, pred):
+		return criterion(label, pred)
 
-mnist = tf.keras.datasets.mnist
+	@tf.function
+	def train_step(x, t):
+		with tf.GradientTape() as tape:
+			preds = model(x)
+			loss = compute_loss(t, preds)
+		grads = tape.gradient(loss, model.trainable_variables)
+		optimizer.apply_gradients(zip(grads, model.trainable_variables))
+		train_loss(loss)
+		train_acc(t, preds)
 
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
-x_train = (x_train.reshape(-1, 28, 28, 1) / 255).astype(np.float32)
-# x_train, x_test = x_train / 255.0, x_test / 255.0
-y_train = np.eye(10)[y_train].astype(np.float32)
-x_train = x_train[:100]
-y_train = y_train[:100]
+		return preds
+
+	@tf.function
+	def test_step(x, t):
+		preds = model(x)
+		loss = compute_loss(t, preds)
+		test_loss(loss)
+		test_acc(t, preds)
+
+		return preds
 
 
+	'''
+	Build model
+	'''
 
-input_layer = tf.keras.layers.Input(shape=(28,28,1))
-output_layer = network(input_layer)
-training_model = Model(inputs=input_layer,outputs=output_layer)
-optim = Adam()
+	network = Vgg16(10)
+	input_layer = Input(shape=(28,28,1))
+	output_layer = network(input_layer)
+	model = Model(inputs=input_layer,outputs=output_layer)
+	model.build(input_shape=(None,28,28,1))
+	criterion = tf.losses.MeanSquaredError()
+	optimizer = tf.keras.optimizers.Adam()
 
-for i in range(100):
-	with tf.GradientTape(watch_accessed_variables=False) as tape:
-		tape.watch(training_model.trainable_variables)
-		preds = training_model(x_train)
-		loss = MSE(preds, y_train)
-		cost = tf.reduce_mean(loss)
-		grads = tape.gradient(loss, training_model.trainable_variables)
-		optim.apply_gradients(zip(grads, training_model.trainable_variables))
-	print(cost)
+
+	'''
+	Load data
+	'''
+
+	mnist = tf.keras.datasets.fashion_mnist
+	(x_train, y_train), (x_test, y_test) = mnist.load_data()
+	x_train = (x_train.reshape(-1, 28, 28, 1) / 255).astype(np.float32)
+	x_test = (x_test.reshape(-1, 28, 28, 1) / 255).astype(np.float32)
+	y_train = np.eye(10)[y_train].astype(np.float32)
+	y_test = np.eye(10)[y_test].astype(np.float32)
+
+
+	'''
+	Train model
+	'''
+
+	epochs = 5
+	batch_size = 2
+	n_batches = x_train.shape[0] // batch_size
+
+	train_loss = tf.keras.metrics.Mean()
+	train_acc = tf.keras.metrics.CategoricalAccuracy()
+	test_loss = tf.keras.metrics.Mean()
+	test_acc = tf.keras.metrics.CategoricalAccuracy()
+
+
+	for epoch in range(epochs):
+
+		_x_train, _y_train = shuffle(x_train, y_train)
+
+		for batch in range(n_batches):
+			start = batch * batch_size
+			end = start + batch_size
+			train_step(_x_train[start:end], _y_train[start:end])
+		preds = test_step(x_test, y_test)
+		print('Epoch: {}, Valid Cost: {:.3f}, Valid Acc: {:.3f}'.format(
+			epoch+1,
+			test_loss.result(),
+			test_acc.result()
+		))
+
+
 
 
 
